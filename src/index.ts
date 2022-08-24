c4c644c08525bc2b7ea0316d3011168a21449bad
default-admin <ashutosh.ukey@circle.com>
2022-08-24 19:00:00 -0400

    fix: added template export message

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ export function pingPackage(msg?: string): string {
     return "You have succesfully pinged the package"
 }
 
-export const aSecret: string = "This has one keyword";
+export const aSecret: string = "My favorite TV show is <placeholder>";
