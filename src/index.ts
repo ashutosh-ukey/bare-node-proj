d8b9b3df14d646d2f425311b8cc6269b2774563c
default-admin <ashutosh.ukey@circle.com>
2022-08-24 14:35:38 -0400

    feat!: function returns string

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-export function pingPackage(msg?: string): void {
+export function pingPackage(msg?: string): string {
     if (msg) { 
         console.log("I received:", msg);
         if (msg.includes("starphish")) console.log(";)");
     }
     else console.log("Hello world (a release workflow!)!");
+
+    return "Return beta"
 }
 
 export const aSecret: string = "This has one keyword";
