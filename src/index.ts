5b873511c893d2c02e7fc60a6987d46439519920
Ashutosh Ukey <ashutosh.ukey@circle.com>
2022-09-27 19:40:07 -0700

    fix: rand change

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function pingPackage(msg?: string): string {
-    return "pong!";
+    return "pong! y";
 }
 
 export const aSecret: string = "My favorite kids cartoon is Spongebob Squarepants";
